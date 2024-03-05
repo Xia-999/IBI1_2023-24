f36d3b2b80815ae722aa5c3d882a384032a88cc0
Shitong <shitong.23@intl.zju.edu.cn>
2024-03-05 11:26:47 +0800

    edit cariables.py

---
+++ Practical4/variables.py
@@ -0,0 +1,13 @@
+a=40
+b=36
+c=30
+d=a-b
+e=b-c
+if d>e:
+    print("the first month's improvement is greater")
+elif d<e:
+    print("the second month's improvement is greater")
+else:
+    print("two months' improvements are equa")
+# if True, the first month's improvement is greater
+# if False, the second month's improvement is greater
