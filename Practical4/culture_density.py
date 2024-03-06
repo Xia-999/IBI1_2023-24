06e014f98257f3122cdf27ecde3e8f7c1793ab85
Shitong <shitong.23@intl.zju.edu.cn>
2024-03-06 14:02:57 +0800

    edit culture_density

--- Practical4/culture_density.py
+++ Practical4/culture_density.py
@@ -1,8 +1,14 @@
+#Function: calculate the culture density
+  #if culture density<=90%:
+    #day=day+1
+  #if culture density>90%:
+    #done
+       
 d=1
 x=5
 while x<=90:
     x*=2
     d+=1
 print("on day "+str(d)+" the cell density goes over 90%.git ")
-print("I can have "+str(d-2)+" days for holiday.")
+print("I can have "+str(d-1)+" days for holiday.")
     
