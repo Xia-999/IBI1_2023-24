44f67d58f301babfc9661c179430b41834909193
Shitong <shitong.23@intl.zju.edu.cn>
2024-03-22 18:36:41 +0800

    edit City Sizes

--- Practical6/City Sizes.py
+++ Practical6/City Sizes.py
@@ -1,26 +1,26 @@
-import matplotlib.pyplot as plt
-uk_cities=[0.56,0.62,0.04,9.7]
+import matplotlib.pyplot as plt  #import matplotlib
+uk_cities=[0.56,0.62,0.04,9.7]   
 china_cities=[0.58,8.4,29.9,22.2]
-uk_cities.sort()
-china_cities.sort()
+uk_cities.sort()  #sort the data in uk_cities in ascending sort order
+china_cities.sort()  #sort the data in china_cities in ascending sort order
 print(uk_cities)
 print(china_cities)
 uk_city=["Stirling","Edinburgh","Glasgow","London"]
 china_city=["Haining","Hangzhou","Beijing","Shanghai"]
 
 plt.figure(1)
-plt.bar(uk_city,uk_cities,color=["green","purple"])
-plt.ylabel("city size")
-plt.title("uk cities")
-for a,b in zip(uk_city,uk_cities):
+plt.bar(uk_city,uk_cities,color=["green","purple"]) #draw a gragh of uk cites'sizes
+plt.ylabel("city size")  #give gragh a y label
+plt.title("uk cities") #give gragh a title
+for a,b in zip(uk_city,uk_cities):  #label the data on the top of bars
     plt.text(a,b,b)
 plt.clf
 
 plt.figure(2)
-plt.bar(china_city,china_cities,color=["grey","pink"])
-plt.ylabel("city size")
-plt.title("china cities")
-for a,b in zip(china_city,china_cities):
+plt.bar(china_city,china_cities,color=["grey","pink"]) #draw a gragh of china cites'sizes
+plt.ylabel("city size")  #give gragh a y label
+plt.title("china cities")  #give gragh a title
+for a,b in zip(china_city,china_cities):  #label the data on the top of bars
     plt.text(a,b,b)
-plt.show()
+plt.show()  # show the graghs
 plt.clf
